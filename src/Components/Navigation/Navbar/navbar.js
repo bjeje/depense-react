33554ae256e748a01a27ead7dd92e0cf13cfa69e
jeje <jerome.botiveau@epitech.eu>
2021-09-06 11:35:19 +0200

    navbar scss + img + navbar component prepare

--- src/Components/Navigation/Navbar/navbar.js
+++ src/Components/Navigation/Navbar/navbar.js
@@ -1,21 +1,27 @@
 import React, {Component, Fragment} from 'react';
 import {Link} from "react-router-dom";
-import './navbar.css';
-
-const Environnement = require('../../../Constants/environment')
-const Env = Environnement.environement
+import './navbar.scss';
+import logoDepense from "../../../Assets/logo/logoDepense.png";
+import {bindActionCreators} from "redux";
+import {connect} from 'react-redux';
+import { getUser } from "../../../Redux/Actions/user.actions";
+import men from "../../../Assets/profil/men.jpg";
+import woman from "../../../Assets/profil/woman.jpg";
 
 class Navbar extends Component {
 
     constructor(props) {
         super(props)
         this.state = {
+            imgWoman: woman,
+            imgMen: men,
+            imgOther: "",
         }
         this.goToLogin = this.goToLogin.bind(this)
     }
 
     componentDidMount() {
-
+        this.props.getUser();
     }
 
     goToLogin(event) {
@@ -31,9 +37,9 @@ class Navbar extends Component {
 
                 <Fragment>
                 { connected ?
-                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <nav className="navbar navbar-expand-lg navbar-light bg-navbar">
                     <div className="container-fluid">
-                        <Link to={"#"}>logo</Link>
+                        <Link to={"/"}><img src={logoDepense} className={"logo-navbar"}/></Link>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
                                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                                 aria-expanded="false" aria-label="Toggle navigation">
@@ -46,13 +52,47 @@ class Navbar extends Component {
                                     <button className="btn btn-outline-success" type="submit">Search</button>
                             </form>
                         </div>
-                        <Link to={"#"}>Profil</Link>
+                        <Link to={"/profile"} className={"last--link"}>{this.props.user.firstName}
+                            { this.props.user.gender === "male" ?
+                                <img src={this.state.imgMen} className={"imgProfile"}/>:null
+                            }
+                            { this.props.user.gender === "woman" ?
+                                <img src={this.state.imgWoman} className={"imgProfile"}/>:null
+                            }
+                            { this.props.user.gender === "other" ?
+                                <img src={this.state.imgWoman} className={"imgProfile"}/>:null
+                            }
+                        </Link>
                     </div>
                 </nav> : null
                 }
+                    {this.props.isLoading?
+                        <p>je charge ...</p>:null
+                    }
+                <div>
+
+                    <p>hey</p> {this.props.user.firstName ?
+                    <p>{this.props.user.login}</p>:null
+                        }
+
+                </div>
             </Fragment>
         )
     }
 }
 
-export default Navbar
+const mapStateToProps = (state) => {
+    console.log(state)
+    return {
+        user: state.userReducer.user,
+        isLoading: state.userReducer.isLoading,
+        error: state.userReducer.error
+    };
+
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return bindActionCreators({getUser: getUser}, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps) (Navbar);
